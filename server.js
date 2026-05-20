1402ce49fa1539fe7df919058e1205928caaf6e6
Abhishek Sharma <its.abhisharmaa@gmail.com>
2026-05-21 01:27:02 +0530

    Fix Express 5 wildcard routing compatibility

--- server.js
+++ server.js
@@ -441,7 +441,7 @@ app.post('/api/chat', async (req, res) => {
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Catch-all route to serve the React app for any other requests (enables client-side routing)
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
